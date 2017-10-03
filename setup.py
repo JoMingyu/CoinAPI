816009286c7b39a225cbd1919b4bee7b37b2c102
PlanB <city7310@naver.com>
2017-10-03 17:05:08 +0900

    [ADD] setup.py, ignores

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='coin-api',
+      version='0.1',
+      url='https://github.com/JoMingyu/Coin-API',
+      license='MIT',
+      author='PlanB',
+      author_email='city7310@naver.com',
+      description='Coins(Digital Currencies) market price API',
+      install_requires=['bs4'])
